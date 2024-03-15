a7ff59a5dcdb2bd514dca605c6399453e37a8c4f
Joey Stipich <stipichj@gmail.com>
2024-03-15 17:53:56 -0400

    EOD 1 commit

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import PawnStars from "./PawnStars.mp3"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine">
+      <div id="interface">
+        <div id="buttonContainer">
+          <button className="drum-pad" id="heater1">
+            Q
+            <audio controls src={PawnStars}></audio>
+          </button>
+          <button className="drum-pad" id="heater2">W</button>
+          <button className="drum-pad" id="heater3">E</button>
+          <button className="drum-pad" id="heater4">A</button>
+          <button className="drum-pad" id="Clap">S</button>
+          <button className="drum-pad" id="Open-HH">D</button>
+          <button className="drum-pad" id="Kick-n'-Hat">Z</button>
+          <button className="drum-pad" id="Kick">X</button>
+          <button className="drum-pad" id="Closed-HH">C</button>
+        </div>
+        <div id="display">
+          <h1>hello</h1>
+        </div>
+      </div>
     </div>
   );
 }
