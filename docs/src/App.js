8e571b775532bbed90726c3a3d2e78420e463b78
Joey Stipich <stipichj@gmail.com>
2024-03-18 13:35:16 -0400

    added components

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,27 +1,52 @@
 import './App.css';
-import PawnStars from "./PawnStars.mp3"
+import { useState } from 'react';
+import PawnStars from "./PawnStars.mp3";
+
+function Display({displayText}){
+
+  return (
+    <div id="display">
+      {displayText}
+    </div>
+  )
+}
+
+function Pad({sample, onClick, padLabel}) {
+  
+  return (
+    <button className="drum-pad" id={sample} onClick={onClick}>
+      {padLabel}
+    </button>
+  )
+}
 
 function App() {
+  
+  const [sample, setSample] = useState('') 
+  const [displayText, setDisplayText] = useState('')
+
+  function handleClick(){
+    setDisplayText({sample})
+  }
+  
   return (
     <div id="drum-machine">
       <div id="interface">
         <div id="buttonContainer">
           <button className="drum-pad" id="heater1">
             Q
-            <audio controls src={PawnStars}></audio>
+            <audio src={PawnStars}></audio>
           </button>
-          <button className="drum-pad" id="heater2">W</button>
-          <button className="drum-pad" id="heater3">E</button>
-          <button className="drum-pad" id="heater4">A</button>
-          <button className="drum-pad" id="Clap">S</button>
-          <button className="drum-pad" id="Open-HH">D</button>
-          <button className="drum-pad" id="Kick-n'-Hat">Z</button>
-          <button className="drum-pad" id="Kick">X</button>
-          <button className="drum-pad" id="Closed-HH">C</button>
+          <Pad sample="heater2" onClick={handleClick} padLabel="W" />
+          <Pad sample="header3" onClick={handleClick} padLabel="E" />
+          <Pad sample="heater4" onClick={handleClick} padLabel="A" />
+          <Pad sample="Clap" onClick={handleClick} padLabel="S" />
+          <Pad sample="Open-HH" onClick={handleClick} padLabel="D" />
+          <Pad sample="Kick-n'-Hat" onClick={handleClick} padLabel="Z" />
+          <Pad sample="Kick" onClick={handleClick} padLabel="X" />
+          <Pad sample="Closed-HH" onClick={handleClick} padLabel="C" />
         </div>
-        <div id="display">
-          <h1>hello</h1>
-        </div>
+        <Display displayText={displayText}/>
       </div>
     </div>
   );
