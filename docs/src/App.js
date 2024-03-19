50b2bcb00787bf8a30f549dc4ecda0ed008c4e7b
Joey Stipich <stipichj@gmail.com>
2024-03-19 17:48:54 -0400

    add audio and keyboard bindings

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import PawnStars from "./PawnStars.mp3";
+import HarmonicHit6 from "./harmonic-hit-6.wav";
 
 function Display({displayText}){
 
@@ -11,7 +11,7 @@ function Display({displayText}){
   )
 }
 
-function Pad({sample, onClick, padLabel}) {
+function Pad({sample, onClick, padLabel, src}) {
   
   function playSample(){
     alert()
@@ -20,7 +20,7 @@ function Pad({sample, onClick, padLabel}) {
   return (
     <button className="drum-pad" id={sample} value={sample} onClick={onClick}>
       {padLabel} {sample}
-      <audio src={PawnStars} id={sample + "audio"} onClick={playSample}></audio>
+      <audio src={src} id={sample + "audio"} className={padLabel} onClick={playSample}></audio>
     </button>
   )
 }
@@ -31,24 +31,36 @@ function App() {
   const [displayText, setDisplayText] = useState('')
 
   function handleClick(e){
-    setDisplayText(e.target.value)
+    
     const x = document.getElementById(e.target.id + "audio")
     x.play();
+  }
+
+  window.addEventListener("keydown", playKB, false)
+
+  function playKB(e){
+    let audio = document.getElementsByClassName(e.key.toUpperCase())
+    //var test = document.getElementsByClassName("Q")
+    //let x = e.key
+    //console.log("HTML element first item: " + test[0], "event key: " + x.toUpperCase())
+    let text = audio[0].id
+    setDisplayText(text.substring(0, text.length-5))
+    audio[0].play();
   }
    
   return (
     <div id="drum-machine">
       <div id="interface">
         <div id="buttonContainer">
-          <Pad sample="heater1" onClick={handleClick} padLabel="Q" />
-          <Pad sample="heater2" onClick={handleClick} padLabel="W" />
-          <Pad sample="header3" onClick={handleClick} padLabel="E" />
-          <Pad sample="heater4" onClick={handleClick} padLabel="A" />
-          <Pad sample="Clap" onClick={handleClick} padLabel="S" />
-          <Pad sample="Open-HH" onClick={handleClick} padLabel="D" />
-          <Pad sample="Kick-n'-Hat" onClick={handleClick} padLabel="Z" />
-          <Pad sample="Kick" onClick={handleClick} padLabel="X" />
-          <Pad sample="Closed-HH" onClick={handleClick} padLabel="C" />
+          <Pad sample="Harmonic Hit 6" onClick={handleClick} padLabel="Q" src={HarmonicHit6} />
+          <Pad sample="heater 2" onClick={handleClick} padLabel="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" />
+          <Pad sample="header 3" onClick={handleClick} padLabel="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" />
+          <Pad sample="heater 4" onClick={handleClick} padLabel="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" />
+          <Pad sample="Clap" onClick={handleClick} padLabel="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" />
+          <Pad sample="Open-HH" onClick={handleClick} padLabel="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" />
+          <Pad sample="Kick-n'-Hat" onClick={handleClick} padLabel="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" />
+          <Pad sample="Kick" onClick={handleClick} padLabel="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" />
+          <Pad sample="Closed-HH" onClick={handleClick} padLabel="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" />
         </div>
         <Display displayText={displayText}/>
       </div>
